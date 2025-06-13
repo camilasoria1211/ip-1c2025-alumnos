3bafb37a6afa5da8b6eecfe5da0c5dc7943aee32
camilasoria1211 <soriacamila52@gmail.com>
2025-06-13 15:10:20 -0300

    funcion getAllImages en services.py

--- app/layers/services/services.py
+++ app/layers/services/services.py
@@ -8,6 +8,13 @@ from django.contrib.auth import get_user
 
 # función que devuelve un listado de cards. Cada card representa una imagen de la API de Pokemon
 def getAllImages():
+    imagenes_pokemon= transport.getAllImages() #imagenes obtenidas de la API. Se encuentran en Transport.py
+    card_pokemon=[]
+    for pokemon in imagenes_pokemon:
+        cards=translator.fromRequestIntoCard(pokemon) #transformamos a card
+        card_pokemon.append(cards)
+    return card_pokemon
+
     # debe ejecutar los siguientes pasos:
     # 1) traer un listado de imágenes crudas desde la API (ver transport.py)
     # 2) convertir cada img. en una card.
